6ae7a501b3a513a26d89a2bb20c3a677a0c07b13
yenarue <yenarue@gmail.com>
2020-05-19 17:47:57 +0900

    style: Buefy 추가 & 프로젝트 테마 css 설정
    https://trello.com/c/QikjpbNR

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,35 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+// Import Bulma's core
+@import "~bulma/sass/utilities/_all";
 
-#nav {
-  padding: 30px;
+// Set your colors
+$primary: #00BFBA;
+$primary-invert: findColorInvert($primary);
+$twitter: #4099FF;
+$twitter-invert: findColorInvert($twitter);
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+// Setup $colors to use as bulma classes (e.g. 'is-twitter')
+$colors: (
+  "white": ($white, $black),
+  "black": ($black, $white),
+  "light": ($light, $light-invert),
+  "dark": ($dark, $dark-invert),
+  "primary": ($primary, $primary-invert),
+  "info": ($info, $info-invert),
+  "success": ($success, $success-invert),
+  "warning": ($warning, $warning-invert),
+  "danger": ($danger, $danger-invert),
+  "twitter": ($twitter, $twitter-invert)
+);
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+// Links
+$link: $primary;
+$link-invert: $primary-invert;
+$link-focus-border: $primary;
+
+// Import Bulma and Buefy styles
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
 </style>
